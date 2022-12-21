9bda466f9560cdf72672c2f000338220e3931870
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-12-21 16:02:34 +0700

    Add Method public void display

--- Exe5_20210140061_Kelompok7/Program.cs
+++ Exe5_20210140061_Kelompok7/Program.cs
@@ -44,7 +44,7 @@ namespace Exe5_20210140061_Kelompok7
             FITRI = el;
         }
 
-        public void remove()
+        public int remove()
         {
             Node tmp;
             Console.WriteLine("\nThe element deleted from the queue is: " + AISAH + "\n");
@@ -57,7 +57,31 @@ namespace Exe5_20210140061_Kelompok7
                 FITRI = null;
             }
             return tmp.info;
+        }
 
+        public void display()
+        {
+            Node Ai;
+            Ai = AISAH;
+
+            if ((Ai == null) && (FITRI == null))
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+
+            Console.WriteLine("\nElements in the queue are....\n");
+
+            while (Ai != FITRI)
+            {
+                Console.WriteLine(Ai.info);
+                Ai = Ai.next;
+            }
+            if (Ai == FITRI)
+            {
+                Console.WriteLine(Ai.info);
+            }
+            Console.WriteLine();
         }
 
 
